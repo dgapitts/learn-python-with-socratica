0f05b932e021b2cdca844e1ee76557183be70e3e
dgapitts <dgapitts@gmail.com>
2020-03-21 19:52:59 +0100

    hello python3 world and string handling

---
+++ 03_strings.py
@@ -0,0 +1,11 @@
+message="meet me tonight"
+print (message)
+print ()
+message="I'm looking for .."
+print (message)
+message='he said "beam me up Scotty"'
+print (message)
+message="""he said "hi" 
+she said "no"
+"""
+print (message)
